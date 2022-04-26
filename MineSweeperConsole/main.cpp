27bf4ce82c4affe9b6b049b8f024ab84b4612c11
devlog98 <dno1998@hotmail.com>
2022-04-26 17:31:03 -0300

    [ADD] Changes to main in order to test methods

--- MineSweeperConsole/main.cpp
+++ MineSweeperConsole/main.cpp
@@ -4,6 +4,21 @@
  *
  */
 
-int main() {
+#include <iostream>
 
+#include "print_header.h"
+#include "print_board.h"
+
+static int size = 26;
+static std::array<char, 26> letters = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
+
+int main() {
+	using namespace Minesweeper;
+
+	print_header();
+
+	print_board(&size, &letters);
+
+	char c;
+	std::cin >> c;
 }
